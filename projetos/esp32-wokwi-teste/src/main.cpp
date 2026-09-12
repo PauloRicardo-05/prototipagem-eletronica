3755ebdeca0f4c2cb09d3dca6e461cb34fcae8ad
Paulo Ricardo <151665091+PauloRicardo-05@users.noreply.github.com>
2026-09-12 20:46:47 -0300

    Finalização do projeto ESP32-Fundamentação

--- projetos/esp32-wokwi-teste/src/main.cpp
+++ projetos/esp32-wokwi-teste/src/main.cpp
@@ -6,8 +6,8 @@
 // CONFIGURAÇÕES DO WI-FI
 // ========================================
 
-const char* ssid = "Gomes";
-const char* password = "r12n15a16p01";
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
 
 
 // ========================================
